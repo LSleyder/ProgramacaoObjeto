8ffc3cdcf463c906a4def0a90874e831890857d0
LSleyder <97415520+LSleyder@users.noreply.github.com>
2022-02-02 13:31:26 -0300

    Add files via upload

---
+++ caTeste/caTeste/Teste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caTeste
+{
+    internal class Teste
+    {
+        private int basis;
+        private int altura;
+
+        public void dadosRetângulo() 
+        {
+            Console.WriteLine("base = " + basis + " altura = " + altura);
+            Console.ReadLine();
+        }
+        public void setBasis(int novoValor)
+        {
+            basis = novoValor;
+        }
+        public void setAltura(int novoValor)
+        {
+            altura = novoValor;
+        }
+    }
+}
